c4eb871445d4b5e5aeb4608c0a76b1ae2808bcf7
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-07 14:46:53 +0800

    added json files, and .gitignore

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee.js")
+
+describe(`Employee`, ()=>{
+    it(`throws an error when given no parameters`, ()=>{
+        expect(()=>{
+            new Employee()
+        }).toThrow(`Error! No parameters passed to constructor!`)
+    })
+})
