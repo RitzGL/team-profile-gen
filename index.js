407c55897e4d1f2d7565d3973e90ec547629e024
Ritz GL <maurigarzonlara@hotmail.com>
2021-05-14 13:54:52 +0800

    added questions for manager, employee type, engineer and intern

--- index.js
+++ index.js
@@ -1,31 +1,100 @@
 const inquirer = require('inquirer');
 
-const employeeQuestions = [
+const managerQuestions = [
+    
     {
-      type: 'input',
-      name: 'list',
-      message: 'Select employee type: ',
-      choices: ["Engineer", "Manger", "Inter"],
+        type: 'input',
+        name: 'name',
+        message: 'Enter manager\'s name: ',
     },
     {
-      type: 'input',
-      name: 'name',
-      message: 'Enter employee name: ',
+        type: 'input',
+        name: 'id',
+        message: 'Enter manager\'s id: ',
     },
     {
-      type: 'input',
-      name: 'id',
-      message: 'Enter employee id: ',
+        type: 'input',
+        name: 'email',
+        message: 'Enter manager\'s email:',
     },
     {
-      type: 'input',
-      name: 'email',
-      message: 'Enter employee email:',
-      when: function (answers) {
-        return answers;
-      },
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter manager\'s office number: ',
+        when: function (answers) {
+            return answers;
+        },
+    },   
+];
+
+// choosing which kind of employee to create 
+let employeeType = [
+    {
+        type: 'list',
+        name: 'employeeType',
+        message: 'Select employee type: ',
+        choices: ["Engineer", "Intern"],
     },
-  ];
+];
+
+// engineer class questions
+const engineerQuestions = [ 
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter engineer\'s name: ',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter engineer\'s id: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter engineer\'s email:',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter intern\'s school: ',
+        when: function (answers) {
+            return answers;
+        },
+    },
+];
+
+const internQuestions = [ 
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter intern\'s name: ',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter intern\'s id: ',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter intern\'s email:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter intern\'s email: ',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter engineer\'s github: ',
+        when: function (answers) {
+            return answers;
+        },
+    },
+];
+
 
 //   need to inquirer.prompt here
 // START
