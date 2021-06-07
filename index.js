2fa4d053648955700576ada4ff20b5b5354c3593
Ritz GL <maurigarzonlara@hotmail.com>
2021-06-07 11:54:58 +0800

    render functionality added, debug

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
+const render = require('./lib/helper/profile-gen')
 // get the type of employee to create from user
 async function getEmployeeType(){
     let employeeType = await inquirer.prompt([
@@ -92,7 +93,7 @@ async function getManagerInfo(){
             message: 'Enter manager\'s email:',
         },
         {
-            type: 'input',
+            type: 'number',
             name: 'officeNumber',
             message: 'Enter manager\'s office number: ',
             when: function (answers) {
@@ -123,17 +124,23 @@ async function callAgain(){
 // populate array based on employee type
 async function getEmployees(employees){
     // get employee type
+    let empArray = [...employees];
     let type = await getEmployeeType();
     // invoke methods depending on what returns from getEmployeeType
+    // switch case might have to be wrapped in another async function,
+    // given that the program always reaches the bottom, and returns, potentially breaking things
+    // in the function that invokes getEmployees()
+    // i could declare two arrays?? one that gets assigned once asynchronous things resolve (the one being passed in)
+    // and another one that gets assigned the value (the array awaiting for getEmployees to resolve)
     switch(type.employeeType){
         case "Engineer":
             try {
                 let engineer = await getEngineerInfo();
                 let engineerObj = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github);
-                employees.push(engineerObj);
+                empArray.push(engineerObj);
                 let recursion = await callAgain();
                 if(recursion.callAgain === "Yes"){
-                    getEmployees(employees);
+                    return await getEmployees(empArray);
                 }
             } catch (error) {
                 console.error(error);
@@ -144,13 +151,13 @@ async function getEmployees(employees){
             try {
                 let intern = await getInternInfo();
                 let internObj = new Intern(intern.name, intern.id, intern.email, intern.school);
-                employees.push(internObj);
+                empArray.push(internObj);
                 let recursion = await callAgain();
                 if(recursion.callAgain === "Yes"){
-                    getEmployees(employees);
+                    return await getEmployees(empArray);
                 }
             } catch (error) {
-                console.error(error);   
+                console.error(error);
             }
             break;
 
@@ -158,10 +165,10 @@ async function getEmployees(employees){
             try {
                 let manager = await getManagerInfo();
                 let managerObj = new Manager(manager.name, manager.id, manager.email, manager.officeNumber);
-                employees.push(managerObj);
+                empArray.push(managerObj);
                 let recursion = await callAgain();
                 if(recursion.callAgain === "Yes"){
-                    getEmployees(employees);
+                   return await getEmployees(empArray);
                 }             
             } catch (error) {
                 console.error(error);
@@ -171,17 +178,26 @@ async function getEmployees(employees){
         default:
             console.error("Class not found!");
     }
-    console.log(employees);
-    return employees;
+    // console.log(empArray);
+    return empArray;
 }
 
 async function main(){
-    let employees = [];
-    employeess = await getEmployees(employees);
-    // console.log(employees);
+    // problem might be trying to assign and pass in the same array.
+    // and when trying to log it, it comes back as undefined, or with its contents changed from async
+    // need to look into this more, and fix by EOD tomorrow
+    // i could do this:
+        // let asyncEmployees = []
+        // let employees = await getEmployees(asyncEmployees)
+        // console.log(employees)
+    let asyncEmployees = [];
+    let employees = await getEmployees(asyncEmployees);
+    console.log(employees);
 }
 
-main();
+console.log(render())
+
+// main();
 // getEmployees();
 //   need to inquirer.prompt here
 // START
